e4feb4a6cd58312afc1db401ae9be3a6e4f14522
piyush_qumar <piyushkumar5271@gmail.com>
2022-12-31 17:44:52 +0530

    random commits

---
+++ java/alpha/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        short a=344;
+        int x=6;
+        float f=456.7f;
+        double d=45.65456;
+        char ch='A';
+        long l=43234l;
+        
+
+
+        // int m1,m2,m3;
+        Scanner s = new Scanner(System.in);
+        int m1 = s.nextInt();
+        int m2 = s.nextInt();
+        int m3 = s.nextInt();
+        int sum=m1+m2+m3;
+        System.out.println((sum/3)*1);
+        
+
+    }
+}
---
+++ java/alpha/src/wel.java
@@ -0,0 +1,6 @@
+public class wel {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int rt;
+    }
+}
