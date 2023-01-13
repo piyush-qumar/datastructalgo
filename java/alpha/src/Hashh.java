6185a95efb703b0e1f25c872f691e3a12b08c44f
piyush kumar <piyushkumar5271@gmail.com>
2023-01-13 11:36:14 +0530

    new codes

--- java/alpha/src/Hashh.java
+++ java/alpha/src/Hashh.java
@@ -1,5 +1,6 @@
 
 //import java.util.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -8,22 +9,37 @@ public class Hashh {
 
         int n;
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> str = new ArrayList<String>();
         HashMap<String, HashMap<String, String>> hm = new HashMap<String, HashMap<String, String>>();
         HashMap<String, String> hm1 = new HashMap<String, String>();
         System.out.println("Enter the number of employees");
         n = sc.nextInt();
         for (int i = 0; i < n; i++) {
-            System.out.println("Enter the company name");
-            String cname = sc.next();
-            System.out.println("Enter the employee name");
+            System.out.println("Enter the name of the employee");
             String name = sc.next();
-            System.out.println("Enter the employee id");
+            System.out.println("enter the id");
             String id = sc.next();
-            hm1.put("Employee Name", name);
-            hm1.put("Employee Id", id);
-            hm.put(cname, hm1);
+            str.add(name);
+            System.out.println(str);
+            hm1.put(str.get(i), id);
         }
-        // System.out.println(hm1);
+        // System.out.println(str);
+        // for (String s : str) {
+        // hm1.put(s, s.length());
+        // }
+        System.out.println(hm1);
+        // for (int i = 0; i < n; i++) {
+        System.out.println("Enter the company name");
+        String cname = sc.next();
+        // System.out.println("Enter the employee name");
+        // String name = sc.next();
+        // System.out.println("Enter the employee id");
+        // String id = sc.next();
+        // hm1.put("Employee Name", name);
+        // hm1.put("Employee Id", id);
+        hm.put(cname, hm1);
+        // }
+        // // System.out.println(hm1);
         System.out.println(hm);
         sc.close();
     }
