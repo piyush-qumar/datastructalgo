c2f73bb05349fa5c75a49ee5a9d39dfa43486ca3
Piyush Kumar <piyushkumar5271@gmail.com>
2026-01-27 22:05:18 +0530

    cookie solution

--- java/alpha/src/Greedy_Algo/AssignCookie.java
+++ java/alpha/src/Greedy_Algo/AssignCookie.java
@@ -1,4 +1,37 @@
 package Greedy_Algo;
 
+import java.util.Arrays;
+import java.util.Scanner;
+class AssignCookieSol {
+    public int solution(int[] s, int[] c) {
+        Arrays.sort(s);
+        Arrays.sort(c);
+        int l = 0;
+        int h = 0;
+        while(l<s.length && h<c.length) {
+            if(c[h]>=s[l]) {
+                l++;
+            }
+            h++;
+        }
+        return l;
+    }
+}
 public class AssignCookie {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int x,y;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        int[] s =new int[x];
+        int [] c = new int[y];
+        for(int i = 0;i<x;i++) {
+            s[i] = sc.nextInt();
+        }
+        for(int j = 0;j<y;j++) {
+            c[j] = sc.nextInt();
+        }
+        AssignCookieSol sol = new AssignCookieSol();
+        System.out.println(sol.solution(s, c));
+    }
 }
